2f8f2c148c86cb4fe7a3eb784328dd2bb4e9f836
sunilkumarkommu <48117909+sunilkumarkommu@users.noreply.github.com>
2022-11-30 18:00:53 +0530

    updated main

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
 terraform {
-resource "aws_instance" "example" {
-  count         ="${var.number}"
-  ami           ="${var.ami}"
-  instance_type ="${var.type}"
-  key_name      = "masterslave"
-  
-  tags = {
-    Name = "Sample${count.index}"
+   required_providers {
+    aws = {
+      source = "./ec2.tf"
+    }
   }
-}
   cloud {
     organization = "ojas"
 
