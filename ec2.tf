619398635d428225517960c9612758876a63ca26
sunilkumarkommu <48117909+sunilkumarkommu@users.noreply.github.com>
2022-11-30 18:00:13 +0530

    added ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "example" {
+  count         ="${var.number}"
+  ami           ="${var.ami}"
+  instance_type ="${var.type}"
+  key_name      = "masterslave"
+  
+  tags = {
+    Name = "Sample${count.index}"
+  }
+  
+  }
